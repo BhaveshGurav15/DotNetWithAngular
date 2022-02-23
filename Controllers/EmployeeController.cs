e1d843eea81edb979fd6a47adcca2a32ecd541ec
My Lappy <bhavesh.gurav@nitorinfotech.com>
2022-02-23 20:33:42 +0530

    First Commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ngWithJwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        [Route("GetUserData")]
+        //[Authorize(Policy = Policies.User)]
+        public IActionResult GetUserData()
+        {
+            return Ok("This is Employee");
+        }
+    }
+}
